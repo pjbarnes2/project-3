e2639e5f8c5dca85db513118140cbd82b561e106
pjbarnes2 <pjb5805@psu.edu>
2023-04-20 12:22:12 -0400

    got it fixed slay

--- src/p3-card.js
+++ src/p3-card.js
@@ -6,27 +6,20 @@ export class P3Card extends LitElement {
 
   static get properties() {
     return {
-      badges: { type: Array }, 
-     
+      
       name: {
         type: String,
         reflect: true
       }, 
       image: {type: String},
-      description: {
+      companylogo:{type: String},
+      name: {type:String},
+      summary: {
         type: String,
       }, 
-      opened: {type: Boolean, reflect: true},
-      link: {type: String}, 
-      author: {type: String}, 
-      authorImage: {type: String}, 
-      time: {type: String},
-      steps: {type: Array}, 
-      stepDescription: {type: String}
-
+    
     }
   }
-
 
   static get styles(){
     return css`
@@ -35,141 +28,116 @@ export class P3Card extends LitElement {
 
     display: block;
     }
- .wrapper {
-        
-        background-color: #3e98d3;
-        color: white;
-        padding: 2px 2px 2px 20px;
-        text-align: center-left;
-        width: 800px;
-        height: auto;
-        border-radius: 5px;
-      }
-
-      .title{
-        width: auto;
-        color: black;
-        font-family: sans-serif;
-        background-color: lightblue;
-        text-align: left;
-        font-weight:bold;
-        font-size: .6cm;
-      }
-      
-      .body{
+    .wrapper {
+  width: 400px;
+  border: 2px solid black;
+  display: inline-flex;
+  background-color: black;
+}
+.body{
         font-family: sans-serif;
         background-color: #cfe6f4;
         padding: auto;
         text-align: left;
         height: auto;
       }
-      
-      .image {
-        width: 20px;
-        text-align: left;
-        
-      }
-      
-      input {
-        font-size: 20px;
-        font-weight: bold;
-        border: none;
-        border-bottom: 1px solid black;
-        transition: all .3s ease-in-out;
-      }
-      
-      
-      
-      .header {
-        text-align: left;
-        font-weight: bold;
-        font-size: 2rem; /* scales relatively */
-        
-        
-      }
-      
-      
-      details{
-        margin-left: 24px;
-        font-size: 10px;
-        padding: 10px;
-        text-align: left;
-      }
-      
+.image {
+  width: 400px;
+}
+.companylogo
+{
+  display: block;
+  margin-left: auto;
+  width: 100px;
+}
+.header {
+    line-height: 1em;
+    font-size: 50px;
+    font-family: "Monument Extended Ultrabold";
+    letter-spacing: 0.04em;
+    text-transform: uppercase;
+    color: white;
+    text-align: center;
+}
+
+.header h4 {
+  margin: 16px;
+  font-style: normal;
+}
+
+
+.name {
+    line-height: 1.2em;
+    font-size: 40px;
+    fill: rgb(59, 59, 59);
+    font-family: "Bryso Letter Gothic Bold";
+    color: rgb(130, 73, 73);
+    text-align: left;
+}
+.summary {
+margin: 0;
+    font-family: "Open Sans", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: white;
+    text-align: left;
+}
+
+
+
+
+@media only screen and (max-width: 425px) {
+  .wrapper {
+    font-weight: normal;
+  }
+  .wrapper .header h3 {
+    font-size: 12px;
+  }
+  .wrapper .header h4 {
+    font-size: 10px !important;
+  }
+  details {
+    display: none;
+  }
+}
+
     `;
   }
-
-
-
-  toggleEvent(e) {
-    console.log(this.opened);
-    console.log(this.shadowRoot.querySelector('details').getAttribute('open'));
-    const state = this.shadowRoot.querySelector('details').getAttribute('open') === '' ? true : false;
-    console.log(state);
-    this.opened = state;
-  } //listens
-
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if(propName === 'opened') {
-        this.dispatchEvent(new CustomEvent('change-open-state', 
-        {
-          composed: true, //event occured in shadowroot but you want it to bubble up through page
-          bubbles: true,
-          cancelable: false,
-          detail: { 
-            value: this[propName]} 
-          }));
-          console.log(`${propName} changed. oldValue: ${oldValue}`);
+  constructor() {
+    super();
+    this.name = "Topher";
+    this.summary = "Powerful Being";
+    this.companylogo = "Cool Guy";
   
-        }
-  
-        });
-        
-
-
-    }
-  
-
-
-
+  }
 
 
     render() {
       return html`
-    <div>
- 
+    
+<div class="wrapper">
+  <div class="container">
+   
+    <div class="header">
+      <h3>Topher</h3>
+      
     </div>
-    <div>
-    <div class="wrapper">
-  
-  <div class="badge">
-    <div class="title">${this.title}
-      <div class="image">
-      <simple-icon icon="editor:bubble-chart">${this.image}</simple-icon>
-      </div>
- <details class="details" .opened="${this.opened}" @toggle="${this.toggleEvent}">
- 
+     <img class="image" src="https://i.kym-cdn.com/entries/icons/mobile/000/043/203/tophercover.jpg" />
+<img class="companylogo" src= "https://i.kym-cdn.com/entries/icons/mobile/000/043/203/tophercover.jpg" />
+    < class="name">
+      <summary class="summary">
+        <ul>
+        Topher Lastname
+        </ul>
       <div>
-        <p class="description">${this.description}
-  
-      </p>
-      <div class="link">
-      <a href=${this.link}>${this.link}</a>
-
-      </div>
-      <div>
-        ${this.author} 
-        <simple-icon icon="face">${this.image}</simple-icon>
-      </div>
-      ${this.time}
-      ${this.steps}
-  </details>
-      </div>
+        <ul>
+          killed god and took his place</li>
+          owns 70% of all rae dunn mugs</li>
+        </ul>
+       
+        </summary>
     </div>
-    </div>
-    </div>
-  </div>
  
     `;
   }
